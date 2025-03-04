46bcf0ef05fccde1c29ce4ca7cd6687d94c4e325
Bartosz Biały <s30759@pjwstk.edu.pl>
2025-03-04 13:24:14 +0100

    Zad3

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Witaj, Swiecie!");
-Console.WriteLine("Bartosz");
-Console.WriteLine("Helom");
+static double GetAverage(int[] arr) {
+    int result = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result += arr[i];
+    }
+    return result;
+}
+
+int[] arr = { 15,12,54,32 };
+Console.WriteLine(GetAverage(arr));
